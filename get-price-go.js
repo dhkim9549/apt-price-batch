a24c8d2d3a7047daec4795da04470f45ce5acba3
root <dhkim95@hotmail.com>
2026-08-02 04:40:06 +0000

    fisrt commit

---
+++ get-price-go.js
@@ -0,0 +1,183 @@
+import { open, appendFile } from "node:fs/promises";
+import { MongoClient } from "mongodb";
+import "dotenv/config";
+
+main();
+
+async function main() {
+  console.log("start main...");
+  console.log(process.argv);
+  let fileNm = process.argv[2] ?? "input-file.csv";
+  await insertPrc(fileNm);
+}
+
+async function insertPrc(fileNm) {
+  console.log("fileNm = " + fileNm);
+
+  const client = new MongoClient(process.env.MONGODB_URI);
+  await client.connect();
+  console.log("Connected successfully to server");
+  const db = client.db("dbApt");
+  const collection = db.collection("colAptPrc");
+
+  const file = await open(fileNm);
+  let i = 0;
+  let failCnt = 0;
+
+  for await (const line of file.readLines()) {
+    i++;
+    if (line.startsWith("보증번호")) {
+      continue;
+    }
+
+    let s = new String(line);
+
+    const w = s.split("\t");
+
+    let apt = {};
+    apt.addr = w[6];
+    apt.hsTynm = w[4];
+    apt.basisYr = w[5];
+
+    const basisYr = apt.basisYr;
+
+    await appendFile("r-list-go-output.txt", "\n" + w[1], "utf8");
+
+    if (apt.hsTynm != "아파트") continue;
+
+    const addr_w = apt.addr.split(",");
+    if (addr_w.length <= 1) continue;
+
+    let aptNm = addr_w[addr_w.length - 1].slice(0, -1);
+
+    const stnm_addr_w = addr_w[0].split(" ");
+
+    const stnmAddr2 =
+      stnm_addr_w[stnm_addr_w.length - 2] +
+      " " +
+      stnm_addr_w[stnm_addr_w.length - 1];
+    apt.stnmAddr2 = stnmAddr2;
+
+    const dongho_w = addr_w[1].split(" ");
+
+    let dongNm = "",
+      dongNm2 = "";
+    let hoNm = "",
+      hoNm2 = "";
+
+    for (const wt of dongho_w) {
+      if (dongNm == "" && wt.endsWith("동") && !wt.startsWith("(")) {
+        dongNm = wt;
+        dongNm = dongNm.replaceAll(/[동()]/g, "");
+        dongNm2 = dongNm + "동";
+      }
+      if (hoNm == "" && wt.endsWith("호")) {
+        hoNm = wt;
+        hoNm = hoNm.replaceAll(/[호()]/g, "");
+        hoNm2 = hoNm + "호";
+      }
+    }
+
+    console.log("\n\n\n\n\n");
+    console.log({ i, s, aptNm, stnmAddr2, dongNm, hoNm });
+
+    let aptPrcAr = await collection
+      .find({
+        basisYr: basisYr,
+        stnmAddr2: stnmAddr2,
+        dongNm: { $in: [dongNm, dongNm2] },
+        hoNm: { $in: [hoNm, hoNm2] },
+      })
+      .toArray();
+
+    if (aptPrcAr.length > 1) {
+      console.log(aptPrcAr);
+      aptPrcAr = aptPrcAr.filter(
+        (element) => element.aptNm.indexOf(aptNm.slice(0, 2)) >= 0,
+      );
+    }
+
+    if (aptPrcAr.length == 0) {
+      aptPrcAr = await collection
+        .find({
+          basisYr: basisYr,
+          stnmAddr2: stnmAddr2,
+          hoNm: { $in: [hoNm, hoNm2] },
+        })
+        .toArray();
+      if (aptPrcAr.length > 0) console.log("flag 1");
+    }
+
+    if (aptPrcAr.length != 1 && hoNm != "") {
+      aptPrcAr = await collection
+        .find({
+          basisYr: basisYr,
+          stnmAddr2: stnmAddr2,
+          dongNm: { $regex: dongNm, $options: "i" },
+          hoNm: { $in: [hoNm, hoNm2] },
+        })
+        .toArray();
+      if (aptPrcAr.length > 0) console.log("flag 2");
+    }
+
+    if (aptPrcAr.length != 1 && hoNm != "") {
+      aptPrcAr = await collection
+        .find({
+          basisYr: basisYr,
+          stnmAddr2: stnmAddr2,
+          dongNm: { $in: [dongNm, dongNm2] },
+          hoNm: { $regex: hoNm, $options: "i" },
+        })
+        .toArray();
+      if (aptPrcAr.length > 0) console.log("flag 3");
+    }
+
+    if (aptPrcAr.length != 1 && hoNm != "") {
+      aptPrcAr = await collection
+        .find({
+          basisYr: basisYr,
+          stnmAddr2: stnmAddr2,
+          dongNm: { $regex: dongNm, $options: "i" },
+          hoNm: { $regex: hoNm, $options: "i" },
+        })
+        .toArray();
+      if (aptPrcAr.length > 0) console.log("flag 4");
+    }
+
+    if (aptPrcAr.length != 1 && hoNm != "") {
+      aptPrcAr = await collection
+        .find({
+          basisYr: basisYr,
+          stnmAddr2: stnmAddr2,
+          hoNm: { $regex: hoNm, $options: "i" },
+        })
+        .toArray();
+      if (aptPrcAr.length > 0) console.log("flag 5");
+    }
+
+    console.log("aptPrcAr.length = " + aptPrcAr.length);
+
+    let price = "";
+
+    if (aptPrcAr.length == 1) {
+      price = aptPrcAr[0].price;
+    } else {
+      failCnt++;
+    }
+
+    await appendFile("r-list-go-output.txt", "," + basisYr + "," + price, "utf8");
+
+    if (i % 1 == 0) {
+      console.log({ failCnt });
+      console.log({ aptNm });
+      console.log({ dongho_w });
+      console.log("apt = " + JSON.stringify(apt, null, 2));
+      console.log("aptPrcAr = " + JSON.stringify(aptPrcAr, null, 2));
+      console.log(new Date());
+    }
+  }
+
+  client.close();
+
+  return "done.";
+}
---
+++ insert-price-go.js
@@ -0,0 +1,77 @@
+import { open } from "node:fs/promises";
+import { MongoClient } from "mongodb";
+import "dotenv/config";
+
+main();
+
+async function main() {
+  console.log("start main...");
+  console.log(process.argv);
+  let fileNm = process.argv[2];
+  await insertPrc(fileNm);
+}
+
+async function insertPrc(fileNm) {
+  console.log("fileNm = " + fileNm);
+
+  const client = new MongoClient(process.env.MONGODB_URI);
+  await client.connect();
+  console.log("Connected successfully to server");
+  const db = client.db("dbApt");
+  const collection = db.collection("colAptPrc");
+
+  const file = await open(fileNm);
+  let i = 0;
+  for await (const line of file.readLines()) {
+    i++;
+    if (line.startsWith('"기준연도"')) {
+      continue;
+    }
+
+    let s = new String(line);
+    s = line.substring(1, s.length - 1);
+    s = s.replaceAll('","', "|");
+    const w = s.split("|");
+    let apt = {};
+    apt.basisYr = w[0];
+    apt.basisMm = w[1];
+    apt.dongCd = w[2];
+    apt.stnmAddr = w[3];
+    apt.city = w[4];
+    apt.sggu = w[5];
+    apt.epmn = w[6];
+    apt.dnri = w[7];
+    apt.spcd = w[8];
+    apt.bno = w[9];
+    apt.buno = w[10];
+    apt.spnm = w[11];
+    apt.aptNm = w[12];
+    apt.dongNm = w[13];
+    apt.hoNm = w[14];
+    apt.area = Number(w[15]);
+    apt.price = Number(w[16]);
+    apt.aptCd = w[17];
+    apt.dongNmCd = w[18];
+    apt.hoNmCd = w[19];
+
+    const w2 = apt.stnmAddr.split(" ");
+    apt.stnmAddr2 = w2[w2.length - 2] + " " + w2[w2.length - 1];
+
+    let aptFilter = structuredClone(apt);
+    delete aptFilter.price;
+
+    // await collection.updateOne(aptFilter, { $set: apt }, { upsert: true });
+    await collection.insertOne(apt);
+
+    if (i % 1000 == 0) {
+      console.log("i = " + i);
+      console.log(s);
+      console.log("apt = " + JSON.stringify(apt, null, 2));
+      console.log(new Date());
+    }
+  }
+
+  client.close();
+
+  return "done.";
+}
